effb103edcfe453a85f9138829e72caff01fa6ef
Wasiq Nazir <nwasiq@gmail.com>
2020-09-23 19:57:48 +0500

    sweep test done

--- sign_request_test.go
+++ sign_request_test.go
@@ -10,7 +10,8 @@ import (
 
 var signingPin string
 var withdrawReqJson string
-var withdrawSigned string
+var sweepReqJson string
+var key *ECKey
 
 func SignWithdrawRequestSetup(t *testing.T) {
 	var reqBuf strings.Builder
@@ -28,6 +29,30 @@ func SignWithdrawRequestSetup(t *testing.T) {
 	}
 
 	withdrawReqJson = reqBuf.String()
+}
+
+func SignSweepRequestSetup(t *testing.T) {
+	var reqBuf strings.Builder
+	signingPin = "Was1qWas1q"
+
+	sweepReqFile, err := os.Open("fixtures/sweep_request.json")
+	if err != nil {
+		t.Error(err)
+	}
+	defer sweepReqFile.Close()
+
+	_, err = io.Copy(&reqBuf, sweepReqFile)
+	if err != nil {
+		t.Error(err)
+	}
+
+	sweepReqJson = reqBuf.String()
+
+	ecKey, wifErr := FromWIF("cUhedoiwPkprm99qfUKzixsrpN3w6wT2XrrMjqo3Yh1tHz8ykVKc")
+	if wifErr != nil {
+		t.Errorf("Error extracting key from WIF: %s", wifErr)
+	}
+	key = ecKey
 }
 
 func ParseResult(str string) (*SignatureData, error) {
@@ -98,6 +123,28 @@ func TestWithdraw(t *testing.T) {
 		1: []string{"3045022100be9d194a967a91c8f77db4c6bf0bd3d2fdb2235cd6a78328954c448e255aa17d02207bb89868300c594838b5fedad6f01810dbf9d2c97e44e267c63b121cab2dcdeb"},
 	}
 	reqObj, reqErr := ParseSignatureResponse(withdrawReqJson)
+	if reqErr != nil {
+		t.Errorf("Parsing the unsigned JSON threw an error: %s", reqErr)
+	}
+	// parse the JSON output - must output valid json and not throw error
+	sigObj, parseErr := ParseResult(signatureReq)
+	if parseErr != nil {
+		t.Errorf("Parsing the signed JSON threw an error: %s", parseErr)
+	}
+	compareSignedRequest(expectedSigs, reqObj, sigObj, t)
+}
+
+func TestSweep(t *testing.T){
+	SignSweepRequestSetup(t)
+	signatureReq, signErr := SignRequestJsonWithKey(key, sweepReqJson)
+	if signErr != nil {
+		t.Errorf("Signing threw an error: %s", signErr)
+	}
+	// signatures we expect
+	expectedSigs := expSignedInputs{
+		0: []string{"30440220480a498bb662ec3993cf9e429143acb40d1b9c29e2d1256bbbb4c26506e9708902205f41f66c2ec5340f7d1dcbee1940b67faf768160c9c7e1ef3d199794bbc69d13"},
+	}
+	reqObj, reqErr := ParseSignatureResponse(sweepReqJson)
 	if reqErr != nil {
 		t.Errorf("Parsing the unsigned JSON threw an error: %s", reqErr)
 	}
